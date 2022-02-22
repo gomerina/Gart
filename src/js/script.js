40c925e211cf2db7dde83d568e689ee3accee732
gomerina <KuzovkovSer52reg@yandex.ru>
2022-02-22 15:12:55 +0300

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -134,9 +134,19 @@ stagesTop = stages.getBoundingClientRect().top + document.body.scrollTop;
 let footer = document.querySelector('.footer'),
 footerTop = footer.getBoundingClientRect().top + document.body.scrollTop;
 
-
+let scrollPos = 0;
 // Переключение индикатора
-document.addEventListener('scroll', function(){
+$(window).scroll(function(){
+	// Шапка
+	var st = $(this).scrollTop();
+	if (st > scrollPos){
+		$('.header').removeClass('show');
+		$('.header').addClass('hidden');
+	} else {
+		$('.header').addClass('show');
+		$('.header').removeClass('hidden');
+	}
+	scrollPos = st;
 	// Бурение
 	if (window.pageYOffset >= 0) {
 		indicatorItemDrilling.classList.add('active');
@@ -296,3 +306,37 @@ $('.indicator__item').click(function(){
 	$('.indicator').removeClass('active');
 	$('html').removeClass('lock');
 })
+// Маска на инпут телефона
+window.addEventListener("DOMContentLoaded", function() {
+	[].forEach.call( document.querySelectorAll('input[type="tel"]'), function(input) {
+		var keyCode;
+		function mask(event) {
+			event.keyCode && (keyCode = event.keyCode);
+			var pos = this.selectionStart;
+			if (pos < 3) event.preventDefault();
+			var matrix = "+7 (___) ___-__-__",
+			i = 0,
+			def = matrix.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, ""),
+			new_value = matrix.replace(/[_\d]/g, function(a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+			});
+			i = new_value.indexOf("_");
+			if (i != -1) {
+				i < 5 && (i = 3);
+				new_value = new_value.slice(0, i)
+			}
+			var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+				function(a) {
+					return "\\d{1," + a.length + "}"
+				}).replace(/[+()]/g, "\\$&");
+			reg = new RegExp("^" + reg + "$");
+			if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+			if (event.type == "blur" && this.value.length < 5)  this.value = ""
+		}
+	input.addEventListener("input", mask, false);
+	input.addEventListener("focus", mask, false);
+	input.addEventListener("blur", mask, false);
+	input.addEventListener("keydown", mask, false)
+});
+});
